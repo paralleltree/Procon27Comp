0daa090c570197c93b68643ab31e07829f7c7775
Paralleltree <paralleltree@outlook.com>
2016-08-20 22:12:39 +0900

    Add project files.

---
+++ Procon27Comp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Procon27Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Procon27Comp/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Procon27Comp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Procon27Comp")]
+[assembly: AssemblyCopyright("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fcc222b4-74b9-400d-8742-8d4aa2197aa6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
