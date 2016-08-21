8397ebda47672474fc38cad4c3c72d695418e58f
Paralleltree <paralleltree@outlook.com>
2016-08-21 11:07:41 +0900

    Add base component classes

---
+++ Procon27Comp/Components/Vertex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Numerics;
+
+namespace Procon27Comp.Components
+{
+    /// <summary>
+    /// 座標と内角を持った図形の頂点を表します。
+    /// </summary>
+    public class Vertex
+    {
+        /// <summary>
+        /// 頂点の座標を取得します。
+        /// </summary>
+        public Vector2 Location { get; }
+
+        /// <summary>
+        /// 頂点が持つ内角を取得します。
+        /// </summary>
+        public double Angle { get; }
+
+        /// <summary>
+        /// 頂点の座標と内角から、<see cref="Vertex"/>クラスの新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="loc">頂点の座標</param>
+        /// <param name="angle">頂点の内角</param>
+        public Vertex(Vector2 loc, double angle)
+        {
+            Location = loc;
+            Angle = angle;
+        }
+    }
+}
--- Procon27Comp/Internal/VectorHelper.cs
+++ Procon27Comp/Internal/VectorHelper.cs
@@ -22,5 +22,17 @@ namespace Procon27Comp.Internal
         {
             return Math.Acos((Vector2.Dot(a, b) / (a.Length() * b.Length())));
         }
+
+        /// <summary>
+        /// 反時計回りの2ベクトルからへこんでいる頂点かどうかを判定します。
+        /// </summary>
+        /// <param name="prev">対象頂点の前の頂点を起点とするベクトル</param>
+        /// <param name="next">対象頂点の次の頂点を終点とするベクトル</param>
+        /// <returns>対象頂点が前の頂点から次の頂点へ向かうベクトルの左側にあればTrue、そうでなければFalse。</returns>
+        public static bool IsConcaveVertex(Vector2 prev, Vector2 next)
+        {
+            var composite = Vector2.Add(prev, next);
+            return Vector3.Cross(new Vector3(composite, 0), new Vector3(prev, 0)).Z < 0;
+        }
     }
 }
