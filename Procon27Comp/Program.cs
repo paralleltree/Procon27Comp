402c029c082dd8525f6d14b5d21e1c9121a4049c
Paralleltree <paralleltree@outlook.com>
2016-10-06 18:25:14 +0900

    コマンドライン引数をとった

--- Procon27Comp/Program.cs
+++ Procon27Comp/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Procon27Comp.Components;
+using Procon27Comp.Solvers;
+
 namespace Procon27Comp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var puzzle = new PuzzleReader(args[0]).Read();
+            var solver = new StupidSolver(puzzle);
+            solver.Solve();
+            foreach (var s in solver.Solutions)
+            {
+                s.DumpToImage(string.Format("out.png"));
+            }
         }
     }
 }
