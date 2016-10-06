7a3cc96b58adcfef99c00901e6d3cbe752b1dbca
Paralleltree <paralleltree@outlook.com>
2016-10-06 18:02:39 +0900

    Add PolygonHelper class

---
+++ Procon27Comp/Internal/PolygonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Drawing;
+using Procon27Comp.Components;
+
+namespace Procon27Comp.Internal
+{
+    public static class PolygonHelper
+    {
+        public static Vertesaur.Polygon2 GetPolygon(this PuzzleComponent pc)
+        {
+            return new Vertesaur.Polygon2(pc.Vertexes.Select(p => new Vertesaur.Point2(p.Location.X, p.Location.Y)));
+        }
+
+        public static Vertesaur.Polygon2 GetPolygon(this GpcWrapper.Polygon poly)
+        {
+            return new Vertesaur.Polygon2(poly.Contour.Zip(poly.ContourIsHole, (p, q) => new Vertesaur.Ring2(p.Vertex.Select(r => new Vertesaur.Point2(r.X, r.Y)), q)));
+        }
+
+        public static GpcWrapper.Polygon GetGpcPolygon(this Vertesaur.Polygon2 poly)
+        {
+            var gp = new GpcWrapper.Polygon();
+            foreach (var ring in poly)
+                gp.AddContour(new GpcWrapper.VertexList(ring.Select(p => new PointF((float)p.X, (float)p.Y)).ToArray()), false);
+            return gp;
+        }
+    }
+}
