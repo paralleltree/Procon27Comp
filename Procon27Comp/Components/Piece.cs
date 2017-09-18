ab875403c8ee70452d5af3cb7589c297991631e1
Paralleltree <paralleltree@outlook.com>
2017-09-18 17:38:01 +0900

    MergedPieceクラスを追加

--- Procon27Comp/Components/Piece.cs
+++ Procon27Comp/Components/Piece.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
 
@@ -26,7 +27,7 @@ namespace Procon27Comp.Components
         /// <param name="offsetX">X軸方向の移動距離</param>
         /// <param name="offsetY">Y軸方向の移動距離</param>
         /// <returns>平行移動によってできるピース</returns>
-        public Piece Offset(float offsetX, float offsetY)
+        public virtual Piece Offset(float offsetX, float offsetY)
         {
             return new Piece(Vertexes.Select(p => new Vector2(p.Location.X + offsetX, p.Location.Y + offsetY)));
         }
@@ -37,9 +38,43 @@ namespace Procon27Comp.Components
         /// <param name="src">回転変換を適用するピース</param>
         /// <param name="rad">回転角</param>
         /// <returns>回転変換が適用されたピース</returns>
-        public Piece Rotate(float rad)
+        public virtual Piece Rotate(float rad)
         {
             return new Piece(Vertexes.Select(p => p.Location.Rotate(rad)));
+        }
+
+        public virtual void DrawToImage(System.Drawing.Graphics g, System.Drawing.Pen pen)
+        {
+            this.GetPolygon().DrawToImage(g, pen);
+        }
+    }
+
+    /// <summary>
+    /// 複数の<see cref="Piece"/>が結合している1つのピースを表します。
+    /// </summary>
+    public class MergedPiece : Piece
+    {
+        public List<Piece> ComponentPieces { get; private set; }
+
+        public MergedPiece(IEnumerable<Vector2> points, IEnumerable<Piece> components) : base(points)
+        {
+            ComponentPieces = components.SelectMany(p => p is MergedPiece ? (p as MergedPiece).ComponentPieces : new List<Piece>() { p }).ToList();
+        }
+
+        public override Piece Offset(float offsetX, float offsetY)
+        {
+            return new MergedPiece(base.Offset(offsetX, offsetY).Vertexes.Select(p => p.Location), ComponentPieces.Select(p => p.Offset(offsetX, offsetY)));
+        }
+
+        public override Piece Rotate(float rad)
+        {
+            return new MergedPiece(base.Rotate(rad).Vertexes.Select(p => p.Location), ComponentPieces.Select(p => p.Rotate(rad)));
+        }
+
+        public override void DrawToImage(Graphics g, Pen pen)
+        {
+            foreach (Piece item in ComponentPieces)
+                item.DrawToImage(g, pen);
         }
     }
 }
