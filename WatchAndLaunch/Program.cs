71be8aadbd5a8803f18a1c5764d4eb023d458faa
Paralleltree <paralleltree@outlook.com>
2017-10-04 20:10:14 +0900

    ラーメン行きたい

--- WatchAndLaunch/Program.cs
+++ WatchAndLaunch/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Procon27Comp.WatchAndLaunch
 {
@@ -10,6 +11,18 @@ namespace Procon27Comp.WatchAndLaunch
     {
         static void Main(string[] args)
         {
+            string monitorTarget = args[0];
+            string launchTarget = args[1];
+
+            var watcher = new FileSystemWatcher()
+            {
+                Path = Path.GetDirectoryName(monitorTarget),
+                Filter = Path.GetFileName(monitorTarget)
+            };
+            watcher.Changed += (s, e) => System.Diagnostics.Process.Start(launchTarget, monitorTarget);
+            watcher.EnableRaisingEvents = true;
+
+            Console.ReadKey();
         }
     }
 }
