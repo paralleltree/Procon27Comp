c20e413f879750154cf61c288da5fcc06c45e1f6
Paralleltree <paralleltree@outlook.com>
2017-10-04 20:09:16 +0900

    ファイルモニタするやつ追加

---
+++ WatchAndLaunch/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WatchAndLaunch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("WatchAndLaunch")]
+[assembly: AssemblyCopyright("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6f671fc5-41a5-43ad-a878-244f5454f77e")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
